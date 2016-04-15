6e374f054bb651596db842032844d69dd6cd5926
ListsOfArrays <ListsOfArrays@users.noreply.github.com>
2016-04-14 20:45:59 -0400

    Added UnitAI & UnitAction class templates.

---
+++ AI-for-Game-Design/Project/Assets/Scripts/Units/UnitAction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Graph;
+
+class UnitAction
+{
+    Unit unitRef;
+
+    public UnitAction(Unit u)
+    {
+        unitRef = u;
+    }
+
+    public void SetAttack(Node n)
+    {
+
+    }
+
+    public void SetMove(Node n)
+    {
+
+    }
+
+    public void Attack(Action callBackFunc)
+    {
+        callBackFunc();
+    }
+
+    public void Move(Action callBackFunc)
+    {
+        ;
+    }
+
+    public void DoAllActions(Action callBackFunc)
+    {
+        Action del = () => { Attack(callBackFunc); };
+        Move(del);
+    }
+}
